da52c5c4dcea943e3fc59ac953dac976a114b11e
klelia <clelia.fradella@gmail.com>
2022-06-28 12:54:52 +0200

    added routing

--- resources/js/components/MainComponent.vue
+++ resources/js/components/MainComponent.vue
@@ -1,15 +1,6 @@
 <template>
     <main>
-         <h1>Sono il Main!</h1>
-         <ul>
-            <li v-for="(post,index) in posts" :key="index">
-                {{post.title}}
-                <a href="#" @click="getoDetail(post.slug, index)">Vedi dettaglio</a>
-                <!-- <span v-if="post.detail">
-                     {{post.detail.slug}}
-                </span> -->
-            </li>
-         </ul>
+        <router-view></router-view>
     </main>
 
 
@@ -19,23 +10,9 @@ export default {
     name: 'MainComponent',
     data(){
         return {
-            posts: [],
+            posts: []
+
         }
-    },
-    methods:{
-        getoDetail(slug,index){
-            axios.get('/api/posts/'+ slug).then((response)=>{
-            console.log(response.data);
-            this.posts[index].detail = response.data;
-            console.log(this.posts[index]);
-        })
-        }
-    },
-    created(){
-        axios.get('/api/posts').then((response)=>{
-            console.log(response.data);
-            this.posts = response.data;
-        })
     }
 
 }
--- resources/js/front.js
+++ resources/js/front.js
@@ -5,7 +5,10 @@ window.Vue = require('vue');
 
 import App from './views/App';
 
+import router from './router.js';
+
  const app = new Vue({
 	el: '#root',
-	render: h => h(App), // renderizziamo App all'avvio di Vue
+	render: h => h(App),
+    router
 });
